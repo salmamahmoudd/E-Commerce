3b6cfeedfd32a635439464c8c959fe831aa436ec
Salma Mahmoud <salmamahmoudd.com>
2025-09-26 15:53:26 +0300

    Deploy Angular app to GitHub Pages

--- docs/environment.ts
+++ docs/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // baseURL ===> real (production) www.amazon.com
     baseURL : 'https://ecommerce.routemisr.com',
-    domain : 'https://salmamahmoudd/E-Commerce',
+    domain : 'https://salmamahmoudd.github.io/E-Commerce',
     production: true
 };
